78cfdc156a8f1214bba87b9eb1296d23c7af70d6
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-03 10:53:22 +0900

    JSX

--- src/js/client.js
+++ src/js/client.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+  constructor() {
+    super();
+    this._name = "Tsutomu (constructor)";
+  }
   render() {
+    let name = "Tsutomu";
     return (
+      <>
+      <h1>Welcome!</h1>
       <h1>It Works!</h1>
+      <h1>It's {name}! {1 + 2}</h1>
+      <h1>It's {this.get_result(3)}!</h1>
+      <h1>It's { ((num) => { return 1 + num; })(3) }!</h1>
+      <h1>It's {this._name}!</h1>
+      </>
     );
+  }
+  get_result(num) {
+    return 1 + num;
   }
 }
 
