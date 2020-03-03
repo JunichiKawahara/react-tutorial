a088eb1568df78c02b26212971ee3e16befcaaa3
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-03 12:42:19 +0900

    Event とデータ変更について

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -7,17 +7,13 @@ export default class Layout extends React.Component {
         super();
         this.state = {title: "Welcome"};
     }
+    changeTitle(title) {
+        this.setState({title});
+    }
     render() {
-        setTimeout(
-            () => {
-                this.setState({title: "Welcome Tsutomu!"});
-            },
-            2000
-        );
         return (
             <>
-            <Header title={this.state.title} />
-            <Header title={"Thank you"} />
+            <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
             <Footer />
             </>
         );
